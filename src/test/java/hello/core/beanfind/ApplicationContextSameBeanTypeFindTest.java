9e84617235b5306d5f58b397a98f2793b12aab6d
Youngwoo Kim <yw.sean.kim@gmail.com>
2023-05-29 10:36:28 +0900

    Tutorial

---
+++ src/test/java/hello/core/beanfind/ApplicationContextSameBeanTypeFindTest.java
@@ -0,0 +1,36 @@
+package hello.core.beanfind;
+
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class ApplicationContextSameBeanTypeFindTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(SameBeanConfig.class);
+
+    @Test
+    @DisplayName("타입으로 조회시 동일 타입 2개 이상")
+    void findBeanByTypeDuplicate() {
+        ac.getBean(MemberRepository.class);
+    }
+
+    @Configuration
+    static class SameBeanConfig {
+        @Bean
+        public MemberRepository memberRepository1() {
+            return new MemoryMemberRepository();
+        }
+
+        @Bean
+        public MemberRepository memberRepository2() {
+            return new MemoryMemberRepository();
+        }
+    }
+}
